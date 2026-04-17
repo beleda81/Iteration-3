728060a40b18019f51eb039336efb64ba2600e3f
beleda81 <157327723+beleda81@users.noreply.github.com>
2026-04-17 10:43:52 -0500

    Update order_service.py

--- service/order_service.py
+++ service/order_service.py
@@ -29,11 +29,11 @@ class OrderService:
         self.order_repo.save(order)
         return order
 
-    # REQUIRED: get_order(order_id)
+    
     def get_order(self, order_id):
         return self.order_repo.get(order_id)
 
-    # REQUIRED: get_order_total(order_id)
+   
     def get_order_total(self, order_id):
         order = self.order_repo.get(order_id)
         if not order:
